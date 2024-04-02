44f5e4d4c7d0bbaceba5553295a6a647fa0c9c76
stgourmelon <steven.gourmelon@etudiant.univ-rennes1.fr>
2024-04-02 11:38:56 +0200

    GET ok - POST PUT DELETE fonctionne mais pas terminé

--- controllers/sectionController.js
+++ controllers/sectionController.js
@@ -3,18 +3,37 @@ const Section = require("../models/sectionModels");
 
 exports.createSection = async (req, res) => {
     try {
-        const { id_livre, numero_section, texte, id_image, type } = req.body;
-        const section = await Section.create({ id_livre, numero_section, texte, id_image, type });
-        res.status(201).json(section);
+      const { idLivre } = req.params;
+      const { numero_section, texte, id_image, type } = req.body;
+
+      const section = await Section.create({ id_livre:Number(idLivre), numero_section, texte, id_image, type });
+      res.status(201).json(section);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+      res.status(500).json({ error: error.message });
     }
 };
 
 exports.getAllSections = async (req, res) => {
     try {
+      const { idLivre } = req.params;
       const sections = await Section.findAll({
         attributes: ["id_livre", "numero_section", "texte", "id_image", "type"],
+        where: {
+          id_livre: idLivre
+        },
+        include: [
+          {
+            model: Resultat,
+            as: 'resultats',
+          },
+          {
+            model: Section,
+            as: 'sections',
+            through: 'association_liaison_section',
+            foreignKey: 'id_section_source',
+            otherKey: 'id_section_destination',
+          }
+        ]
       });
       res.status(200).json(sections);
     } catch (error) {
@@ -24,8 +43,12 @@ exports.getAllSections = async (req, res) => {
 
 exports.getSectionById = async (req, res) => {
     try {
-      const { id } = req.params;
-      const section = await Section.findByPk(id, {
+      const { idLivre, idSection } = req.params;
+      const section = await Section.findOne({
+        where: {
+          id_livre: idLivre,
+          id: idSection
+        },
         include: [
           {
             model: Resultat,
@@ -47,13 +70,14 @@ exports.getSectionById = async (req, res) => {
 
 exports.updateSection = async (req, res) => {
     try {
-      const { id } = req.params;
-      const { id_livre, numero_section, texte, id_image, type } = req.body;
+      const { idLivre, idSection } = req.params;
+      const { numero_section, texte, id_image, type } = req.body;
       await Section.update(
-        { id_livre, numero_section, texte, id_image, type },
+        { id_livre: Number(idLivre), numero_section, texte, id_image, type },
         {
           where: {
-            id,
+            id_livre: idLivre,
+            id: idSection,
           },
         },
       );
@@ -65,10 +89,11 @@ exports.updateSection = async (req, res) => {
   
 exports.deleteSection = async (req, res) => {
 try {
-    const { id } = req.params;
+    const { idLivre, idSection } = req.params;
     await Section.destroy({
     where: {
-        id,
+        id_livre: idLivre,
+        id: idSection
     },
     });
     res.status(200).json({ message: "Section deleted" });
