35a29c1d0f451fe8dde6d50bfb04d6362db390cf
maximefurious <maxime.boujeant@gmail.com>
2024-04-04 11:11:05 +0200

    fix/ cors ?

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const userRoutes = require("./routes/userRoutes");
 const livreRoutes = require("./routes/livreRoutes");
@@ -13,18 +14,29 @@ const levenschteinRoutes = require("./routes/levenschteinRoutes");
 const app = express();
 
 app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: "http://193.168.146.103:8081", // Autorise les requêtes depuis cette origine
+    methods: ["GET", "POST", "OPTIONS"], // Autorise ces méthodes HTTP
+    allowedHeaders: ["Content-Type", "Authorization"], // Autorise ces en-têtes personnalisés
+    credentials: true, // Autorise l'envoi de cookies avec la demande
+    maxAge: 86400, // Cache les résultats de la requête pré-vol (OPTIONS) pendant 24 heures
+  }),
+);
 
 const secretKey = "sklLeevR0FHz5ha%2ys#";
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin",
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin",
+//   );
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Max-Age", "1800");
+//   next();
+// });
 
 app.use((req, res, next) => {
   if (
