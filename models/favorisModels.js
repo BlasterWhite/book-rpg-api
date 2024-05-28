dd6aa15d786c82a87c7fb3cecd9034398f4c53a4
maximefurious <maxime.boujeant@gmail.com>
2024-05-28 22:16:57 +0200

    feat/ include livre into favoris response

--- models/favorisModels.js
+++ models/favorisModels.js
@@ -1,52 +1,62 @@
-const { DataTypes } = require("sequelize");
+const {DataTypes} = require("sequelize");
 const sequelize = require("../db/db");
 
 const User = require("./userModels");
 const Livre = require("./livreModels");
 
 const Favoris = sequelize.define(
-  "favori",
-  {
-    id_utilisateur: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: User,
-        key: "id",
-      },
+    "favori",
+    {
+        id_utilisateur: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: User,
+                key: "id",
+            },
+        },
+        id_livre: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Livre,
+                key: "id",
+            },
+        },
     },
-    id_livre: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: Livre,
-        key: "id",
-      },
+    {
+        schema: "bookrpg",
+        tableName: "favori",
+        defaultScope: {
+            attributes: {
+                exclude: [],
+            },
+        },
     },
-  },
-  {
-    schema: "bookrpg",
-    tableName: "favori",
-    defaultScope: {
-      attributes: {
-        exclude: [],
-      },
-    },
-  },
 );
 
 Favoris.belongsTo(User, {
-  foreignKey: "id_utilisateur",
-  targetKey: "id",
-  as: "user",
+    foreignKey: "id_utilisateur",
+    targetKey: "id",
+    as: "user",
 });
 User.hasMany(Favoris, {
-  foreignKey: "id_utilisateur",
-  as: "favoris",
+    foreignKey: "id_utilisateur",
+    as: "favoris",
+});
+
+Favoris.belongsTo(Livre, {
+    foreignKey: "id_livre",
+    targetKey: "id",
+    as: "livre",
+});
+Livre.hasMany(Favoris, {
+    foreignKey: "id_livre",
+    as: "favoris",
 });
 
 Favoris.getAttributes = () => {
-  return ["id", "id_utilisateur", "id_livre"];
+    return ["id", "id_utilisateur", "id_livre"];
 };
 
 module.exports = Favoris;
