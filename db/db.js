67d7133955f94803aa477159ffac3157a6acd9e8
maximefurious <93995791+maximefurious@users.noreply.github.com>
2024-05-30 12:57:45 +0200

    update/ increase maximum pool to optimise Transactions

--- db/db.js
+++ db/db.js
@@ -1,29 +1,36 @@
-const { Sequelize } = require("sequelize");
+const {Sequelize} = require("sequelize");
 const env = {
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
 };
 
 const sequelize = new Sequelize(
-  `postgres://${env.user}:${env.password}@${env.host}:${env.port}/${env.database}`,
-  {
-    define: {
-      timestamps: false,
+    `postgres://${env.user}:${env.password}@${env.host}:${env.port}/${env.database}`,
+    {
+        define: {
+            timestamps: false,
+
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000,
+        }
     },
-  },
 );
 
 // Test the connection
 sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((error) => {
-    console.error("Unable to connect to the database:", error);
-  });
+    .authenticate()
+    .then(() => {
+        console.log("Connection has been established successfully.");
+    })
+    .catch((error) => {
+        console.error("Unable to connect to the database:", error);
+    });
 
 module.exports = sequelize;
