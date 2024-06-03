93fd68c8de4594f4a23db45c4f97f581efab6546
maximefurious <93995791+maximefurious@users.noreply.github.com>
2024-06-03 10:00:44 +0200

    refact/ imageController.js

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,135 +1,93 @@
 const Image = require("../models/imageModels");
 const sequelize = require("../db/db");
 
-exports.getAllImages = async (req, res) => {
-    let transaction;
-    try {
-        transaction = await sequelize.transaction();
-        const images = await Image.findAll({transaction});
-        await transaction.commit();
-        res.status(200).json(images);
-    } catch (error) {
-        if (transaction) await transaction.rollback();
-        res.status(500).json({error: error.message});
-    }
-};
-
-exports.getOneImage = async (req, res) => {
-    let transaction;
-    try {
-        transaction = await sequelize.transaction();
-        const image = await Image.findByPk(req.params.id, {transaction});
-        await transaction.commit();
-        res.status(200).json(image);
-    } catch (error) {
-        if (transaction) await transaction.rollback();
-        res.status(500).json({error: error.message});
-    }
-};
-
 exports.createImage = async (req, res) => {
-    let transaction;
     try {
         if (!req.body.prompt) {
             return res.status(400).json({error: "Please provide a prompt"});
         }
-        transaction = await sequelize.transaction();
-        fetch("https://api.getimg.ai/v1/essential/text-to-image", {
-            method: "POST",
-            headers: {
-                accept: "application/json",
-                "content-type": "application/json",
-                authorization: `Bearer ${process.env.KEY}`,
-            },
-            body: JSON.stringify({
-                prompt: req.body.prompt.toString(),
-            }),
-        })
-            .then((response) => response.json())
-            .then(async (response) => {
-                if (response.error) {
-                    if (transaction) transaction.rollback();
-                    return res.status(400).json({error: response.error});
-                }
-                const image = await Image.create({image: response.image}, {transaction});
-                await transaction.commit();
-                res.status(201).json(image);
-            })
-            .catch((err) => {
-                if (transaction) transaction.rollback();
-                res.status(500).json({error: err.message});
+
+        const result = await sequelize.transaction(async t => {
+            const response = await fetch("https://api.getimg.ai/v1/essential/text-to-image", {
+                method: "POST",
+                headers: {
+                    accept: "application/json",
+                    "content-type": "application/json",
+                    authorization: `Bearer ${process.env.KEY}`,
+                },
+                body: JSON.stringify({
+                    prompt: req.body.prompt.toString(),
+                }),
             });
+            const data = await response.json();
+            if (data.error) {
+                return {
+                    error: data.error,
+                    code: 400,
+                };
+            }
+            const image = await Image.create({image: data.image}, {transaction: t});
+            return {
+                image,
+                code: 201,
+            };
+        });
+        if (result.error) {
+            return res.status(result.code).json({error: result.error});
+        }
+        res.status(201).json(result.image);
     } catch (error) {
-        if (transaction) await transaction.rollback();
         res.status(500).json({error: error.message});
     }
 };
 
 exports.createImageB64 = async (req, res) => {
-    let transaction;
     try {
         if (!req.body.image) {
             return res.status(400).json({error: "Please provide an image"});
         }
-        transaction = await sequelize.transaction();
-        const image = await Image.create({image: req.body.image}, {transaction});
-        await transaction.commit();
-        res.status(201).json(image);
+
+        const result = await sequelize.transaction(async t => {
+            return await Image.create({image: req.body.image}, {transaction: t});
+        });
+        res.status(201).json(result);
     } catch (error) {
-        if (transaction) await transaction.rollback();
         res.status(500).json({error: error.message});
     }
 };
 
 exports.createImageURL = async (req, res) => {
-    let transaction;
     try {
         if (!req.body.url) {
             return res.status(400).json({error: "Please provide an url"});
         }
-        transaction = await sequelize.transaction();
-        const image = await Image.create({image: req.body.url}, {transaction});
-        await transaction.commit();
-        res.status(201).json(image);
+        const result = await sequelize.transaction(async t => {
+            return await Image.create({image: req.body.url}, {transaction: t});
+        });
+        res.status(201).json(result);
     } catch (error) {
-        if (transaction) await transaction.rollback();
-        res.status(500).json({error: error.message});
-    }
-};
-
-exports.updateImage = async (req, res) => {
-    let transaction;
-    try {
-        transaction = await sequelize.transaction();
-        const image = await Image.findByPk(req.params.id, {transaction});
-        if (!image) {
-            return res.status(404).json({error: "Image not found"});
-        }
-        await image.update(req.body, {
-            where: {
-                id: req.params.id,
-            },
-        }, {transaction});
-        await transaction.commit();
-        res.json(image);
-    } catch (error) {
-        if (transaction) await transaction.rollback();
         res.status(500).json({error: error.message});
     }
 };
 
 exports.deleteImage = async (req, res) => {
-    let transaction;
     try {
-        transaction = await sequelize.transaction();
-        const image = await Image.findByPk(req.params.id, {transaction});
-        await image.destroy({
-            transaction
+        const {id} = req.params;
+        const result = await sequelize.transaction(async t => {
+            const image = await Image.findByPk(id, {transaction: t});
+            if (!image) {
+                return {
+                    code: 404,
+                    message: "Image not found",
+                };
+            }
+            return await image.destroy({transaction: t});
         });
-        await transaction.commit();
-        res.json({message: `Image ${req.params.id} deleted`});
+        if (result.error) {
+            return res.status(result.code).json({error: result.message});
+        }
+        res.status(200).json({message: `Image ${id} deleted`});
     } catch (error) {
-        if (transaction) await transaction.rollback();
         res.status(500).json({error: error.message});
     }
 };
--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -3,12 +3,9 @@ const imageController = require("../controllers/imageController");
 
 const router = express.Router();
 
-router.get("/", imageController.getAllImages);
-router.get("/:id", imageController.getOneImage);
 router.post("/", imageController.createImage);
 router.post("/b64image", imageController.createImageB64);
 router.post("/url", imageController.createImageURL);
-router.put("/:id", imageController.updateImage);
 router.delete("/:id", imageController.deleteImage);
 
 module.exports = router;
